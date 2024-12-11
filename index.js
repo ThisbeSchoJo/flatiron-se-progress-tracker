2f99f07109b56c4b57332917025c823132b402ec
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2024-12-11 10:32:42 +0000

    Add code to index.js, add array of tasks, select DOM elements, add calculateProgress, add localStorage to save user's progress

---
+++ index.js
@@ -0,0 +1,27 @@
+//Array of tasks
+const tasks = [
+    { name: "Phase 0", completed: true},
+    { name: "Phase 1", completed: false},
+    { name: "Phase 2", completed: false},
+    { name: "Phase 3", completed: false},
+    { name: "Phase 4", completed: false},
+    { name: "Phase 5", completed: false},
+];
+
+//Select DOM elements
+const taskList = document.getElementById("task-list");
+const progressBar = document.getElementById("progress-bar");
+const progressText = document.getElementById("progress-text");
+
+//Add an event listener to each checkbox
+//when checkbox is toggled, 
+//update completed property for corresponding task
+//and recalculate the progress
+
+const calculateProgress = () => {
+    const completedTasks = tasks.filter(task => task.completed).length;
+    return (completedTasks / tasks.length) * 100
+}
+
+//Save user's progress with local storage
+localStorage.setItem("tasks", JSON.stringify(tasks));
